13ea8e2369e59731c56271705b514382b70cb838
MeLean <milen820430@gmail.com>
2014-10-02 23:37:16 +0300

    The windows forms for the games

---
+++ Game2D/Game2D/GameSource.cs
@@ -0,0 +1,19 @@
+﻿namespace Game2D
+{
+    using System;
+    using System.Windows.Forms;
+
+    class GameSource
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MenuForm());
+        }
+    }
+}
---
+++ Game2D/Game2D/MenuForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game2D
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            TitleForm TitleForm = new TitleForm();
+            TitleForm.ShowDialog();
+
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            GameForm Game = new GameForm();
+
+            if (Game.ShowDialog() == DialogResult.Yes)
+            {
+                GameCompletedForm Completed = new GameCompletedForm();
+                Completed.ShowDialog();
+            }
+            else
+            {
+                GameOverForm Over = new GameOverForm();
+                Over.ShowDialog();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
