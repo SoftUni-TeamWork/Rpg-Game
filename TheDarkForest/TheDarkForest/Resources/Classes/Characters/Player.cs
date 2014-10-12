dfa5698350f94d92c24614211dd83111865caaf7
MeLean <milen820430@gmail.com>
2014-10-12 21:53:04 +0300

    Added Classes/Characters/... and Classes/Items/...

---
+++ TheDarkForest/TheDarkForest/Resources/Classes/Characters/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheDarkForest.Resources.Classes.Characters
+{
+    class Player : Character
+    {
+        public Player(string id, int healthPoints, int attackPoints, int defencePoints, int currentLevel, Position position) 
+            : base(id, healthPoints, attackPoints, defencePoints, currentLevel, position)
+        {
+        }
+    }
+}
